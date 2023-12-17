c586f6ce9e3d2fddb8d39336c422b98ba9cb9b0a
ryo0421 <ryo.u.forbusiness@gmail.com>
2023-12-17 16:57:02 +0900

    first commit

---
+++ src/main/java/com/example/TodoApplicationForQuickStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplicationForQuickStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplicationForQuickStudyApplication.class, args);
+	}
+
+}
